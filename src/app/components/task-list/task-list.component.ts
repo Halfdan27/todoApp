10b265c2a7f42d8ed6bba46bdf7175d95db8350e
Halfdan27 <roman.anm25@gmail.com>
2025-02-17 13:38:06 -0300

    listo el task-list tanto el html como el .ts ademas de agregar un estilo al html con el stlye.css

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Task } from '../../models/task.model';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-task-list',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './task-list.component.css'
 })
 export class TaskListComponent {
+  constructor(private taskService: TaskService) {}
 
+  get tasks(): Task[] {
+    return this.taskService.getTasks();
+  }
+  
+
+   get pendingTasksCount(): number {
+    return this.tasks.filter(t => !t.completed).length;
+  }
+
+  toggleCompletion(id: number): void {
+    this.taskService.toggleTaskCompletion(id);
+  }
+
+  deleteTask(id: number): void {
+    this.taskService.deleteTask(id);
+  }
+  
 }
